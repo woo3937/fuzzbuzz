069933af21ed3c2e33a6d9fdbf248f3000a42fc1
Mike Patterson <michaelandrewpatterson@gmail.com>
2013-12-20 00:50:29 -0600

    Example now takes in user strings

--- examples/example.cpp
+++ examples/example.cpp
@@ -5,6 +5,13 @@
 
 int main() {
 	FuzzBuzz fbuzz;	// declare a FuzzBuzz object
+	std::string input;
 
 	fbuzz.load("sample-input-file.txt");
+
+	while(1) {
+		std::cout << "Enter input string: ";
+		getline(std::cin, input);
+		std::cout << "Best match for \"" << input << "\" = " << "\"" << fbuzz.best(input) << "\"" << std::endl;
+	}
 }
